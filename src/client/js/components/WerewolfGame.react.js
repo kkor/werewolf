5e193437f693e1122ad5cf0b6470fa29625c7184
k k <kiia.korpi@gmail.com>
2016-05-07 03:49:33 +0300

    Fix dead player logic 2

--- src/client/js/components/WerewolfGame.react.js
+++ src/client/js/components/WerewolfGame.react.js
@@ -94,7 +94,7 @@ var WerewolfGame = React.createClass({
     console.log('Render state: ' + JSON.stringify(this.state.gameState));
 
     var content;
-    if (phase === GamePhases.LOBBY || this.state.playerState.state === RoleStates.ALIVE ) {
+    if (phase === GamePhases.LOBBY || this.state.playerState.status === RoleStates.ALIVE ) {
       content = renderGamePhase(phase, role, gameState);
     } else {
       content = <div>You are dead.</div>;
